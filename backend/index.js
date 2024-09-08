d1d4ed5940b7c754dea37269e999244c52cb86bb
arav39marri <aravindmarripelli@gmail.com>
2024-09-08 18:59:03 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -4,24 +4,21 @@ const userModel = require('./userModel');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config()
-const port = 3500;
+const port = process.env.PORT || 3500;  
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+
+app.use(cors({
+  origin: 'https://fullweb-frontend.vercel.app', 
+  methods: ['GET', 'POST', 'DELETE', 'PUT'],
+  credentials: true  
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello from Express');
 });
-
-app.use( cors({
-  origin:['https://fullweb-frontend.vercel.app'] ,
-  methods:['GET', 'POST', 'DELETE','PUT'] ,
-  credentials: true  
-
-}
-));
-
 
 app.post('/create', async (req, res) => {
   try {
