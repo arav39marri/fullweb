170b146ea3a12d3a9fd7d601b23480a490b9e7db
arav39marri <aravindmarripelli@gmail.com>
2024-09-10 14:46:48 +0530

    commited

--- backend/userModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const userSchema = mongoose.Schema({
-  name: String,
-  username: String,
-  age: String,
-  email: String
-});
-module.exports = mongoose.model('User', userSchema);
